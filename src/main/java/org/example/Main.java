a28223913a9d8081cbf010807024e90319975672
DESKTOP-Q6SRV1I\jjoji <jjojin7168@gmail.com>
2026-09-01 18:47:41 +0900

    Initialize

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution(2, 3);
+    }
+}
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        return answer;
+    }
+}
