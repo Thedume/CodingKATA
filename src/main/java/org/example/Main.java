97bb4b718bebffc67d049f3aa237e948d23a9ba4
DESKTOP-Q6SRV1I\jjoji <jjojin7168@gmail.com>
2026-09-01 19:21:30 +0900

    두 숫자의 나눗셈 문제 해결

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
 
-import org.example.solution.Solution_2;
+import org.example.solution.Solution_7;
 
 public class Main {
     public static void main(String[] args) {
-        Solution_2 s = new Solution_2();
+        Solution_7 s = new Solution_7();
 
         System.out.println(s.solution(2, 3));
     }
---
+++ src/main/java/org/example/solution/Solution_7.java
@@ -0,0 +1,7 @@
+package org.example.solution;
+
+public class Solution_7 {
+    public int solution(int num1, int num2) {
+        return num1 * 1000 / num2;
+    }
+}
