3a9fd0bd1b53d2255897925a4c1e87485749bd7a
DESKTOP-Q6SRV1I\jjoji <jjojin7168@gmail.com>
2026-09-02 15:16:37 +0900

    x만큼 간격있는 n개의 숫자 문제 해결

---
+++ src/main/java/org/example/solution/Solution_16.java
@@ -0,0 +1,13 @@
+package org.example.solution;
+
+public class Solution_16 {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+        for (int i = 0; i < n; i++){
+            answer[i] = (long)x * (i+1);
+        }
+
+        return answer;
+    }
+}
