d4e5d06a9d6ef43bdd9672e2d1452f66fa48645e
DESKTOP-Q6SRV1I\jjoji <jjojin7168@gmail.com>
2026-09-02 15:22:19 +0900

    자연수 뒤집어 배열로 만들기 문제 해결

---
+++ src/main/java/org/example/solution/Solution_17.java
@@ -0,0 +1,14 @@
+package org.example.solution;
+
+public class Solution_17 {
+    public int[] solution(long n) {
+        String str = String.valueOf(n);
+        int[] answer = new int[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            answer[i] = str.charAt(str.length() - 1 - i) - '0';
+        }
+
+        return answer;
+    }
+}
