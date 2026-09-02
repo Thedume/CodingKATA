85ec64f36df3b77d09a5952c31c057b8316a022b
DESKTOP-Q6SRV1I\jjoji <jjojin7168@gmail.com>
2026-09-02 15:08:59 +0900

    나머지가 1이 되는 수 찾기 문제 해결

---
+++ src/main/java/org/example/solution/Solution_15.java
@@ -0,0 +1,16 @@
+package org.example.solution;
+
+public class Solution_15 {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
