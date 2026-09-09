dfcdcfe3e8707652a176419eceef3c41fcaf9d1e
Thedume <jjojin7168@gmail.com>
2026-09-09 11:55:45 +0900

    내적 문제 해결

---
+++ src/main/java/org/example/solution/Solution_32.java
@@ -0,0 +1,17 @@
+package org.example.solution;
+
+public class Solution_32 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++){
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(){
+
+    }
+}
